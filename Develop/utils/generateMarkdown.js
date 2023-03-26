97de0dac16e863fbcd7eb89fae4e1604815a2c74
Andrew Breytenbach <ambreyts15@gmail.com>
2023-03-26 19:32:16 -0400

    created a function that returns the license link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -9,7 +9,12 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return `(https://opensource.org/licenses/${license})`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
