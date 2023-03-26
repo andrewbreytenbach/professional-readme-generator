857039e324d5d3d98245989c668ba37fc3c33e61
Andrew Breytenbach <ambreyts15@gmail.com>
2023-03-26 19:23:07 -0400

    created a function that returns a license badge based on which license is passed

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return `![License](https://img.shields.io/badge/license-${license}-green.svg) `;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
